bfef41c2a987bcbf4d6186a35ceb3c48bb7e6f12
CryPan <studio11keyuniverse@gmail.com>
2022-01-25 00:15:12 +1300

    fix

---
+++ Assets/Script/bombcountdown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class bombcountdown : MonoBehaviour
+{
+    float timeLeft = 300.0f;
+    public Text Message; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Debug.Log("bomb" + timeLeft);
+        
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}

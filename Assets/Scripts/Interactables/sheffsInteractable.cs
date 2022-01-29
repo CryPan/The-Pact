0751649382790b6d693723b35f740a2624a7d1e9
CryPan <studio11keyuniverse@gmail.com>
2022-01-29 16:09:41 +1300

    Update Fix 7

---
+++ Assets/Scripts/Interactables/sheffsInteractable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+namespace VHS
+{    
+    public class sheffsInteractable : InteractableBase
+    {
+        bool OpenSheff = true;
+        public PlayableDirector In;
+        public PlayableDirector Out;
+
+        public override void OnInteract()
+        {
+            base.OnInteract();
+
+            if(OpenSheff == true)
+            {
+                Out.Play();
+                OpenSheff = false;
+            }
+            else
+            {
+                In.Play();
+                OpenSheff = true;
+            }
+        }
+    }
+}

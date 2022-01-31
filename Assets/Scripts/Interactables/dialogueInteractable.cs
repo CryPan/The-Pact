9dde8ddfd68f7f7f88ace1617a2775b78b210fbc
CryPan <studio11keyuniverse@gmail.com>
2022-01-31 13:11:31 +1300

    Adding Digalog

---
+++ Assets/Scripts/Interactables/dialogueInteractable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace VHS
+{    
+    public class dialogueInteractable : InteractableBase
+    {
+        bool OpenSheff = true;
+        public string A1;
+        public string B1;
+        public string B2;
+        public string C1;
+        public string C2;
+        public string C3;
+        public string C4;
+        public string D1;
+        public string D2;
+        public string D3;
+        public string D4;
+
+        public override void OnInteract()
+        {
+            base.OnInteract();
+
+        }
+    }
+}
